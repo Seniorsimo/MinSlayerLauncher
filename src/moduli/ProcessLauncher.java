976924de8b991f9cd070fb020a614c953b1d70c8
Simone <seniorsimo@hotmail.it>
2014-08-06 11:23:39 +0200

    Creazione struttura

---
+++ src/moduli/ProcessLauncher.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moduli;
+
+/**
+ *
+ * @author simone
+ */
+public class ProcessLauncher {
+    
+}
---
+++ src/moduli/updater/GameUpdater.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moduli.updater;
+
+/**
+ *
+ * @author simone
+ */
+public class GameUpdater {
+    
+}
